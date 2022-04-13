713eab7d37202f9df9bbce32905bf08d7a252415
athul-23p <athul238p@gmail.com>
2022-04-13 21:16:06 +0530

    added TodoList component

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
-import './todoinput.css';
-export const TodoInput = ({addTodoHandler}) => {
+import "./todoinput.css";
+export const TodoInput = ({ addTodoHandler }) => {
   const [text, setText] = useState("");
 
   return (
     <div>
       <input
-      id="todo-input"
+        id="todo-input"
         type="text"
         placeholder="Enter todo"
+        value={text}
         onChange={(e) => {
           setText(e.target.value);
-        
         }}
       />
-      <button id='add-todo-btn'onClick={() => addTodoHandler(text)}>+</button>
+      <button
+        id="add-todo-btn"
+        onClick={() => {
+
+          addTodoHandler(text);
+          setText('');
+        }}
+      >
+        +
+      </button>
     </div>
   );
 };
